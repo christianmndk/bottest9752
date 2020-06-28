f9f73a51faf7521542d84d5380190070d5db5e8d
toavonhayden <63295986+toavonhayden@users.noreply.github.com>
2020-06-28 16:10:19 +0200

    haha

---
+++ bot.js
@@ -0,0 +1,226 @@
+var auth = require('./auth.json');
+
+// used so the bot can download things
+const https = require('https');
+const fs = require('fs');
+/*
+How to download
+// Find the file type and download it
+filename = "file." + attachment.url.split('.')[attachment.url.split('.').length-1]
+const file = fs.createWriteStream(filename);
+const request = https.get(attachment.url, function(response) {
+    response.pipe(file);
+});
+*/
+
+// Extract the required classes from the required modules
+const { Client, MessageAttachment, MessageManager } = require('discord.js');
+const { spawn } = require('child_process');
+
+// Create an instance of a Discord client
+const client = new Client();
+
+// Create some constants
+const ffmpegFormats = ['avi','flac','flv','gif','m4v','mjpeg','mov','mp2','mp3','mp4','mpeg','nut','oga','ogg','ogv','opus','rawvideo','rm','tta','v64','wav','webm','webp','wv']
+let VoiceChannels = new Map()
+
+// Adding a voice connection
+function addVoiceConnection(connection, message) {
+    var info = new Map()
+    info.set('playing', '')
+    info.set('queue', '')
+    info.set('id', message.member.voice.channel.id)
+    info.set('guild', message.guild.id)
+    info.set('name', message.member.voice.channel.name)
+    info.set('connection', connection)
+    ConnectionID = message.guild.id + message.member.voice.channel.id
+    console.log(ConnectionID)
+    VoiceChannels.set(ConnectionID, info)
+}
+
+// removing a voice connection
+function removeVoiceConnection(ConnectionID) {
+    VoiceChannels.delete(ConnectionID)
+}
+
+client.on('ready', () => {
+    console.log('I am ready!');
+  });
+
+client.on('message', async message => {
+    // If the message is starts with testbot and author is not a bot
+    if (message.content.substring(0, 8) == 'testbot ' && !message.author.bot ) {
+        console.log('recieved command');
+        var args = message.content.substring(8).split(' ');
+        var cmd = args[0];
+        args = args.splice(1);
+
+        switch(cmd) {
+            // testbot rip
+            case 'rip': {
+                // Create the attachment using MessageAttaSchment
+                attachment = new MessageAttachment('https://i.imgur.com/w3duR07.png');
+                // Send the attachment in the message channel with a content
+                message.reply(attachment);
+                break;
+            }
+            // testbot ping
+            case 'ping': {
+                message.reply('pong!');
+                console.log(message)
+                break;
+            }
+            // testbot reply
+            case 'webm': {
+                if (typeof message.attachments.first() === 'undefined') {
+                    message.reply('the command \'webm\' requires a webm attachment sent with the message');
+                    break;
+                }
+                else {
+                    // Go through each attachment
+                    message.attachments.each(attachment => {
+                        // Create an identical attachment as the one in the message and send it back
+                        name = attachment.name.split('.').slice(0, attachment.name.split('.').length-1).join() + ".mp4"
+                        if (!attachment.name.toLowerCase().endsWith('webm')) {
+                            message.reply(attachment.name + ' was not converted because it is not a webm');
+                        }
+                        else {
+                            var ffmpeg = spawn('ffmpeg', ['-y', '-i', attachment.url, '-c:a:v', 'copy' ,'file.mp4'])
+                            ffmpeg.on('close', code => {
+                                if (code == 0) {
+                                    console.log(attachment)
+                                    console.log('Sending converted')
+                                    Converted = new MessageAttachment('./file.mp4', name);
+                                    message.reply(Converted).then(fs.unlink('./file.mp4', er => { if (er) {console.error('An error occurred:\n', er)} })).catch(er => console.error(er));
+                                }
+                                else {
+                                    console.log('ffmpeg failed during conversion')
+                                    message.reply(attachment.name + ' could not be converted because an error happened during conversion');
+                                }
+                            });
+                        }
+                    });
+                }
+                break;
+            }
+            // testbot convert
+            case 'convert' : {
+                var name;
+                // check for new file type
+                if (args.length == 0) { message.channel.send(`${message.author}, you need to provide a new file type for the file`) }
+                else if (!ffmpegFormats.includes(args[0])) {message.channel.send(`${message.author}, the first argument must be a valid file extension like mp4`)}
+                // check if user made a new name otherwise give it the old name 
+                if (args.length > 1 && typeof args[1] === 'string') {
+                    if (args[1].includes('.')) {message.reply('the second argument must be a name which does NOT include a \'.\'. Continuing with standart name')}
+                    else { name = args[1] + '.' + args[0] }
+                }
+                // get the latest file
+                message.channel.messages.fetch({ limit: 10 })
+                    .then(messages => {return messages.filter(m => typeof m.attachments.first() !== 'undefined' && !m.author.bot);})
+                    .then(messages => { 
+                        if (typeof messages.first() === 'undefined') {message.reply('found no pictures 10 messages back, aborting');}
+                        else {
+                            messages.first().attachments.each(attachment => {
+                                // check for name
+                                if (!name) {name = attachment.name.split('.').slice(0, attachment.name.split('.').length-1).join() + '.' + args[0]}
+                                // convert the file
+                                var ffmpeg = spawn('ffmpeg', ['-y', '-i', attachment.url, '-c:a:v', 'copy', 'file.' + args[0]])
+                                console.log(name)
+                                ffmpeg.on('close', code => {
+                                    if (code == 0) {
+                                        console.log(attachment.url)
+                                        console.log('Sending converted')
+                                        Converted = new MessageAttachment('./file.' + args[0], name);
+                                        message.reply(Converted).then(fs.unlink('./file.' + args[0], er => { if (er) {console.error('An error occurred:\n', er)} })).catch(er => console.error(er));
+                                    }
+                                    else {
+                                        console.log('ffmpeg failed during conversion')
+                                        message.reply(attachment.name + 'could not be converted because an error happened during conversion');
+                                    }
+                                });
+                            });
+                        } 
+                    });
+                break;
+            }
+            // testbot test
+            case 'test' : {
+                break;
+            }
+            // Just add any case commands if you want to..
+        }
+    }
+
+    // Voice channel commands
+
+    // If the message is starts with soundbot and author is not a bot
+    if (message.content.substring(0, 9) == 'soundbot ' && !message.author.bot ) {
+        console.log('recieved voice command');
+        // Test to see if the user is in a voicechannel
+        if (message.guild) {
+            if (!message.member.voice.channel) {
+                message.reply('you must be in a voice channel to use that command')
+                return;
+            }
+        }
+        else {
+            message.reply('you must be in a voice channel to use that command')
+            return;
+        }
+
+        var args = message.content.substring(9).split(' ');
+        var cmd = args[0];
+        args = args.splice(1);
+
+        switch(cmd) {
+            // soundbot rip
+            case 'rip': {
+                // Create the attachment using MessageAttaSchment
+                attachment = new MessageAttachment('https://i.imgur.com/w3duR07.png');
+                // Send the attachment in the message channel with a content
+                message.channel.send(`${message.author},`, attachment);
+                break;
+            }
+            // soundbot ping
+            case 'ping': {
+                message.channel.send(`${message.author}, pong!`);
+                break;
+            }
+            // soundbot join
+            case 'join' : {
+                ConnectionID = message.guild.id + message.member.voice.channel.id
+                if (!VoiceChannels.has(ConnectionID)){
+                    if (!(VoiceChannels.get(ConnectionID).get(guild) == message.guild.id)){
+                        const connection = await message.member.voice.channel.join();
+                        addVoiceConnection(connection, message)
+                        console.log(VoiceChannels)
+                    }
+                    else { message.reply('I am playing in another voice channel right now') }
+                }
+                else { message.reply('I am already playing in that channel') }
+                    
+                break;
+            }
+            // soundbot play
+            case 'play' : {
+                const dispatcher = connection.play('./You_Are_Mine-S3RL_(ft Kayliana)--You_Are_Mine_-_S3RL_ft_Kayliana.mp3')
+                break;
+            }
+            // soundbot leave
+            case 'join' : {
+                break;
+            }
+            // soundbot test
+            case 'test' : {
+                break;
+            }
+            // Just add any case commands if you want to..
+        }
+    }
+});
+
+client.on('message', async message => {
+    
+});
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
+client.login(auth.token);
