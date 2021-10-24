37526bdfbc541be48460bf3f2b9490c680517caf
tokb81 <toke.arnfelt@gmail.com>
2021-10-24 11:28:55 +0200

    loop command file

---
+++ commands/loop.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { VoiceChannels, getTime, checkVoice } = require('../scripts/helper');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('loop')
+		.setDescription('Enable and disable looping of the queue'),
+	async execute(interaction) {
+		/*------------- *
+		*  CHECK VOICE  *
+		* -------------*/
+		if (!await checkVoice(interaction, 4)) {
+			return;
+		}
+		const ConnectionId = interaction.guildId;
+		const soundChannel = VoiceChannels.get(ConnectionId)
+
+		/*-------------- *
+		*  ENABLE QUEUE  *
+		* --------------*/
+		if (soundChannel.get('looping')) {
+			await interaction.editReply('Disabeling loop...');
+			soundChannel.set('looping', false);
+		} else {
+			await interaction.editReply('Enabling loop...');
+			soundChannel.set('looping', true);
+			soundChannel.get('queue').push(soundChannel.get('currentVideoInfo'))
+		}
+	},
+};
