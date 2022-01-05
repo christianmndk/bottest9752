1059e40a907bd4083056b755c40e9e2312402ca6
tokb81 <toke.arnfelt@gmail.com>
2022-01-05 11:35:50 +0100

    dont know

--- webInterface/server.js
+++ webInterface/server.js
@@ -2,126 +2,133 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 
+const options = {
+	key: fs.readFileSync('./Certificates/prime.key'),
+	cert: fs.readFileSync('./Certificates/prime.crt')
+};
+
 const publicFolder = './sitePages';
 const errorFolder = './sitePages/Errors';
 var Server;
 
-// module.exports = {
-// 	initServer: function(ports, host = 'localhost') {
-// 		const server = https.createServer((req, res) => {
-// 			console.log('Request for ' + req.url + ' by method ' + req.method);
-// 		
-// 			switch (req.method) {
-// 				case 'GET':
-// 					Server.emit('GET', req, res);
-// 					break;
-// 
-// 				case 'POST':
-// 					Server.emit('POST', req, res);
-// 					break;
-// 
-// 				case 'PUT':
-// 					Server.emit('PUT', req, res);
-// 					break;
-// 
-// 				case 'REST':
-// 					Server.emit('REST', req, res);
-// 					break;
-// 			
-// 				default:
-// 					console.log(`Unknow method recieved: ${req.method}`);
-// 					Server.emit('ERR', '405', req, res);
-// 					break;
-// 			}
-// 		});
-// 
-// 		if (!(ports instanceof Array)) {
-// 			listen(server, ports, host);
-// 		}
-// 		else {
-// 			for (port of ports) {
-// 				listen(server, port, host);
-// 			}
-// 		}
-// 		
-// 		Server = server;
-// 
-// 		/*--------------- *
-// 		*  GET FUNCTIONS  *
-// 		* ---------------*/
-// 
-// 		Server.on('GET', (req, res) => {
-// 			var fileUrl = req.url;
-// 			if (fileUrl == '/') fileUrl = '/index.html';
-// 		
-// 			var filePath = path.resolve(`${publicFolder}${fileurl}`);
-// 			const fileExt = path.extname(filePath);
-// 			if (fileExt == '.html') {
-// 				fs.access(filePath, (err) => {
-// 					if (err) {
-// 						Server.emit('404', req);
-// 						return;
-// 					}
-// 					res.statusCode = 200;
-// 					res.setHeader('Content-Type', 'text/html');
-// 					fs.createReadStream(filePath).pipe(res);
-// 				});
-// 			}
-// 			else if (fileExt == '.css') {
-// 				res.statusCode = 200;
-// 				res.setHeader('Content-Type', 'text/css');
-// 				fs.createReadStream(filePath).pipe(res);
-// 			}
-// 			else {
-// 				Server.emit('404', req);
-// 			}
-// 		});
-// 
-// 		/*--------------- *
-// 		*  PUT FUNCTIONS  *
-// 		* ---------------*/
-// 
-// 		Server.on('PUT', (req, res) => {
-// 			Server.emit('ERR', '501', req, res);
-// 		});
-// 
-// 		/*---------------- *
-// 		*  POST FUNCTIONS  *
-// 		* ----------------*/
-// 
-// 		Server.on('POST', (req, res) => {
-// 			Server.emit('ERR', '501', req, res);
-// 		});
-// 
-// 		/*---------------- *
-// 		*  REST FUNCTIONS  *
-// 		* ----------------*/
-// 
-// 		Server.on('REST', (req, res) => {
-// 			Server.emit('ERR', '501', req, res);
-// 		});
-// 
-// 		/*--------------- *
-// 		*  404 FUNCTIONS  *
-// 		* ---------------*/
-// 
-// 		Server.on('ERR', (error, req, res) => {
-// 			if (!error) error = '404';
-// 			filePath = path.resolve(`./${errorFolder}/${error}.html`);
-// 			fs.access(filePath, (err) => {
-// 				if (err) { // If an unkown error uccurs 
-// 					console.error(`Error: ${error} sent, but no corosponding HTML file could be found`);
-// 					Server.emit('ERR', req, '500')
-// 				}
-// 			});
-// 			res.statusCode = error;
-// 			res.setHeader('Content-Type', 'text/html');
-// 			fs.createReadStream(filePath).pipe(res);
-// 		});
-// 
-// 		return Server;
-// 	}
-// }
+module.exports = {
+	initServer: function(ports, host = 'localhost') {
+		const server = https.createServer(options, (req, res) => {
+			console.log('Request for ' + req.url + ' by method ' + req.method);
+		
+			switch (req.method) {
+				case 'GET':
+					Server.emit('GET', req, res);
+					break;
+
+				case 'POST':
+					Server.emit('POST', req, res);
+					break;
+
+				case 'PUT':
+					Server.emit('PUT', req, res);
+					break;
+
+				case 'REST':
+					Server.emit('REST', req, res);
+					break;
+			
+				default:
+					console.log(`Unknow method recieved: ${req.method}`);
+					Server.emit('ERR', '405', req, res);
+					break;
+			}
+		});
+
+		if (!(ports instanceof Array)) {
+			listen(server, ports, host);
+		}
+		else {
+			for (port of ports) {
+				listen(server, port, host);
+			}
+		}
+		
+		Server = server;
+
+		/*--------------- *
+		*  GET FUNCTIONS  *
+		* ---------------*/
+
+		Server.on('GET', (req, res) => {
+			console.log(req)
+			var fileUrl = req.url;
+			if (fileUrl == '/' || fileurl === undefined) fileUrl = '/index.html';
+			console.log(fileUrl)
+		
+			var filePath = path.resolve(`${publicFolder}${fileUrl}`);
+			const fileExt = path.extname(filePath);
+			if (fileExt == '.html') {
+				fs.access(filePath, (err) => {
+					if (err) {
+						Server.emit('404', req);
+						return;
+					}
+					res.statusCode = 200;
+					res.setHeader('Content-Type', 'text/html');
+					fs.createReadStream(filePath).pipe(res);
+				});
+			}
+			else if (fileExt == '.css') {
+				res.statusCode = 200;
+				res.setHeader('Content-Type', 'text/css');
+				fs.createReadStream(filePath).pipe(res);
+			}
+			else {
+				Server.emit('404', req);
+			}
+		});
+
+		/*--------------- *
+		*  PUT FUNCTIONS  *
+		* ---------------*/
+
+		Server.on('PUT', (req, res) => {
+			Server.emit('ERR', '501', req, res);
+		});
+
+		/*---------------- *
+		*  POST FUNCTIONS  *
+		* ----------------*/
+
+		Server.on('POST', (req, res) => {
+			Server.emit('ERR', '501', req, res);
+		});
+
+		/*---------------- *
+		*  REST FUNCTIONS  *
+		* ----------------*/
+
+		Server.on('REST', (req, res) => {
+			Server.emit('ERR', '501', req, res);
+		});
+
+		/*--------------- *
+		*  404 FUNCTIONS  *
+		* ---------------*/
+
+		Server.on('ERR', (error, req, res) => {
+			if (!error) error = '404';
+			filePath = path.resolve(`./${errorFolder}/${error}.html`);
+			fs.access(filePath, (err) => {
+				if (err) { // If an unkown error uccurs 
+					console.error(`Error: ${error} sent, but no corosponding HTML file could be found`);
+					Server.emit('ERR', req, '500')
+				}
+			});
+			res.statusCode = error;
+			res.setHeader('Content-Type', 'text/html');
+			fs.createReadStream(filePath).pipe(res);
+		});
+
+		return Server;
+	}
+}
 
 /*------------------- *
 *  GENERAL FUNCTIONS  *
@@ -142,4 +149,4 @@ function getFilePath(fileurl, ext='html') {
 
 const self = module.exports;
 
-//Server = self.initServer(8080);
+Server = self.initServer(8080);
